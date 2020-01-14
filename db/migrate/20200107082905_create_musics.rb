9ecf1717e31db205d37b47c4775e87daea269fc6
hgfrtg <jrbevknf@outlook.com>
2020-01-14 21:58:59 +0900

    fix:migrateの順番変更

--- db/migrate/20200107082905_create_musics.rb
+++ db/migrate/20200107082905_create_musics.rb
@@ -1,7 +1,6 @@
 class CreateMusics < ActiveRecord::Migration[5.2]
   def change
     create_table :musics do |t|
-      t.references :user, foreign_key: true
       t.string :title, null: false
       t.string :audio, null: false
       t.text :description
