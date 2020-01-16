0dd45c7a4a1662cfdbcdec5ff6a8330f4f722dfc
hgfrtg <jrbevknf@outlook.com>
2020-01-16 21:04:15 +0900

    add:いいね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,23 @@ class FavoritesController < ApplicationController
     @music = Music.find(params[:music_id])
     favorite = @music.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_back fallback_location: @music
+    # redirect_back fallback_location: @music
+
+      # respond_to do |format|
+      # format.html { redirect_back fallback_location: @music }
+      # format.js
+    # end
   end
 
   def destroy
     @music = Music.find(params[:music_id])
     favorite = current_user.favorites.find_by(music_id: @music.id)
     favorite.destroy
-    redirect_back fallback_location: @music
+    # redirect_back fallback_location: @music
+
+      # respond_to do |format|
+      # format.html { redirect_back fallback_location: @music }
+      # format.js
+    # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'musics#index'
   resources :musics do
   	resources :music_comments, only: [:create, :destroy]
-  	resources :favorites, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
   end
   resources :users, only:[:index, :show, :update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
