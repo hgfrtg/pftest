064b539edfd0ad239d944f956a2d4a09a949e82d
hgfrtg <jrbevknf@outlook.com>
2020-01-20 15:47:39 +0900

    add:フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,27 @@ class User < ApplicationRecord
 	has_many :musics, dependent: :destroy
 	has_many :music_comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
+
+  # フォロー用
+  # 能動的関係
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow
+  # 受動的関係
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
